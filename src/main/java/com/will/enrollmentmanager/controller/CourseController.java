a1ce9aa3eb23595af7c256ddc852127ba91e1c6a
vu linh <xuandiep2389@gmail.com>
2019-05-28 10:34:26 +0700

    done CRUD course

---
+++ src/main/java/com/will/enrollmentmanager/controller/CourseController.java
@@ -0,0 +1,54 @@
+package com.will.enrollmentmanager.controller;
+
+import com.will.enrollmentmanager.exception.ResourceNotFoundException;
+import com.will.enrollmentmanager.model.Course;
+import com.will.enrollmentmanager.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping("/courses/{courseId}")
+    public Course getCourse(@PathVariable Long courseId) {
+        return courseRepository.findById(courseId).orElseThrow(()->new ResourceNotFoundException("CourseId "+courseId+" not found"));
+    }
+
+    @PostMapping("/courses")
+    public Course createCourse(@Valid @RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+
+    @PutMapping("/courses/{courseId}")
+    public Course updateCourse(@PathVariable Long courseId, @Valid @RequestBody Course courseRequest) {
+        return courseRepository.findById(courseId).map(
+                course -> {
+                    course.setName(courseRequest.getName());
+                    course.setDescription(courseRequest.getDescription());
+                    course.setStudents(courseRequest.getStudents());
+                    return courseRepository.save(course);
+                }
+        ).orElseThrow(()->new ResourceNotFoundException("courseId "+courseId+" not found"));
+    }
+
+    @DeleteMapping("/courses/{courseId}")
+    public ResponseEntity<?> deleteCourse(@PathVariable Long courseId) {
+        return courseRepository.findById(courseId).map(course -> {
+            courseRepository.delete(course);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(()-> new ResourceNotFoundException("courseId "+courseId+" not found"));
+    }
+}
